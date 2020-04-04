280a4f3d9dea023c05e38050faa87320cc072f26
unknown <evan.mead@yahoo.com>
2020-04-04 15:00:10 -0500

    Building Tree

--- BinarySearchTree/BinaryTree.cs
+++ BinarySearchTree/BinaryTree.cs
@@ -8,13 +8,23 @@ namespace BinarySearchTree
 {
     public class BinaryTree
     {
-        public Node()
-        {
-            
-        }
+        public Node root = null;
 
-        public void Add(Node data)
+        public void Add(Node leftNode, Node rightNode, int data)
         {
+            Node node = new Node();
+            node.data = data;
+
+            if (root == null)
+            {
+                root = new Node();
+                root.data = data;
+            }
+            else if (data < root.data)
+            {
+                root.leftNode = new Node(root.leftNode, data);
+            }
+
 
         }
     }
